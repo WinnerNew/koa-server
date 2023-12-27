c59cafaa27a4ceec7e426a466710154aa4473530
LiuWenbin <775405088@qq.com>
2023-12-27 17:19:30 +0800

    fix: 更新端口号

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.prefix('/users')
+router.prefix("/users");
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.post("/signin", async (ctx, next) => {
+  let name = ctx.request.body.name || "",
+    password = ctx.request.body.password || "";
+  console.log(`signin with name: ${name}, password: ${password}`);
+  if (name === "koa" && password === "12345") {
+    ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+  } else {
+    ctx.response.body = `<h1>Login failed!</h1>
+      <p><a href="/">Try again</a></p>`;
+  }
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+router.get("/", function (ctx, next) {
+  ctx.body = "this is a users response!";
+});
 
-module.exports = router
+router.get("/bar", function (ctx, next) {
+  ctx.body = "this is a users/bar response";
+});
+
+module.exports = router;
